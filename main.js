95180fde678fc829fa722104cda6416310ae5a14
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-05-05 21:34:08 -0300

    Trabalhando com datas

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
- 
-    const criarTarefa = (evento) => {
 
+const handleNovoItem = (evento) => {
     evento.preventDefault()
 
     const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
 
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+
+    const dataformatada = data.format('DD/MM/YYYY')
+
+
+    const dados = {
+        valor,
+        dataformatada
+    }
+    
+    lista.appendChild(criarTarefa(dados))
+    input.value = " "
+
+}
+
+
+const criarTarefa = ({valor, dataformatada}) => {
+
+
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+    const conteudo = `<p class="content">${dataformatada}** ${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui())
     tarefa.appendChild(BotaoDeleta())
-    lista.appendChild(tarefa)
-    input.value = " "
 
+    return tarefa
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
